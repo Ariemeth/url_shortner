16cd0c6a39b5ef553f4ebcaf7cdd9b362e805a23
Edward Carmack <ariemethacc@gmail.com>
2022-05-14 23:02:00 -0400

    working files with 2 endpoints for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+//#[macro_use]
+//extern crate rocket;
+use rocket::{get, launch, routes};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/monster/<name>")]
+async fn monster(name: &str) -> String {
+    format!("Hello, monster {}!", name)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, monster])
 }
